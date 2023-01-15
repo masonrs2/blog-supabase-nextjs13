e47920cf2f686e89b5b134b91abce6a069640917
Mason <masonsepulveda@gmail.com>
2023-01-14 23:54:09 -0600

    started pricing/testimonials section

---
+++ app/Testimonial.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Testimonial = () => {
+  return (
+    <div>
+        <h1 className="text-white text-8xl">Testimonials</h1>
+    </div>
+  )
+}
+
+export default Testimonial
