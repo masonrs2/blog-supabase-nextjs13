cb2cd2e41f389e71bdd2d8b73d2839c305424974
Mason <masonsepulveda@gmail.com>
2023-01-15 23:00:52 -0600

    edit navbar bg

--- app/Header.jsx
+++ app/Header.jsx
@@ -77,13 +77,13 @@ const Header = () => {
               <ul className="py-1" aria-labelledby="user-menu-button">
                 <div className={`${isAuthenticated ? "" : "hidden"}`}>
                   <li>
-                    <Link href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</Link>
+                    <Link href="account" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Account</Link>
                   </li>
                   <li>
                     <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
                   </li>
                   <li>
-                    <Link href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</Link>
+                    <Link href="/pricing" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Pricing Info</Link>
                   </li>
                 </div>
                 <li>
--- app/account/page.jsx
+++ app/account/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className="w-screen">
+    <div className="w-screen h-screen">
         <div className="">
             <h1 className="text-white">lllllll</h1>
         </div>
